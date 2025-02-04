846c29b8b658de15ac6a9df89d7f8a8f04262e5a
Aaron T <xlr8aaron@gmail.com>
2025-02-03 18:41:39 -0700

    Added flourish logic to sustained notes

--- MainCentral.py
+++ MainCentral.py
@@ -15,23 +15,25 @@ import mido
 import time
 from datetime import datetime
 from MusicalValues import Note
+import asyncio
 
 def init_globals():
     global notes_dict, scale, octave, R_forearm, R_bicep, R_back, L_forearm, L_bicep, L_back, device_dict
     scale = 'C'
-    octave = 2
-    device_dict = {3709404326: {'Device ID': 3709404326, 'Note': '', 'Muscle': 'Right Forearm', 'Active': False, 'Channel': 2},
-                   1723834707: {'Device ID': 1723834707, 'Note': '', 'Muscle': 'Left Forearm', 'Active': False, 'Channel': 3},
-                   1192667876: {'Device ID': 1192667876, 'Note': '', 'Muscle': 'Right Bicep', 'Active': False, 'Channel': 4},
-                   3369251029: {'Device ID': 3369251029, 'Note': '', 'Muscle': 'Left Bicep', 'Active': False, 'Channel': 5},
-                   2821543978: {'Device ID': 2821543978, 'Note': '', 'Muscle': 'Right Lat', 'Active': False, 'Channel': 6},
-                   2497006410: {'Device ID': 2497006410, 'Note': '', 'Muscle': 'Left Lat', 'Active': False, 'Channel': 7}}
+    octave = 3
+    device_dict = {3709404326: {'Device ID': 3709404326, 'Note': '', 'Muscle': 'Right Forearm', 'Active': False, 'Flourish State': False, 'Channel': 2},
+                   1723834707: {'Device ID': 1723834707, 'Note': '', 'Muscle': 'Left Forearm', 'Active': False, 'Flourish State': False,'Channel': 3},
+                   1192667876: {'Device ID': 1192667876, 'Note': '', 'Muscle': 'Right Bicep', 'Active': False, 'Flourish State': False,'Channel': 4},
+                   3369251029: {'Device ID': 3369251029, 'Note': '', 'Muscle': 'Left Bicep', 'Active': False, 'Flourish State': False,'Channel': 5},
+                   2821543978: {'Device ID': 2821543978, 'Note': '', 'Muscle': 'Right Lat', 'Active': False, 'Flourish State': False,'Channel': 6},
+                   2497006410: {'Device ID': 2497006410, 'Note': '', 'Muscle': 'Left Lat', 'Active': False, 'Flourish State': False,'Channel': 7}}
 
     base_note = Note(scale, octave)
     primary_chords, secondary_chords, primary_triads, secondary_triads = base_note.get_related_chords('major')
     # R_forearm['Note'], R_bicep['Note'], R_back['Note'] = primary_chords
     # L_forearm['Note'], L_bicep['Note'], L_back['Note'] = secondary_chords
-    chords = primary_chords+secondary_chords
+    chords = primary_chords + secondary_chords
+    # print(chords)
     chord_index = 0
     for device in device_dict.values():
         device['Note'] = chords[chord_index]
@@ -45,22 +47,8 @@ def init_globals():
             number = (base_note.notes.index(note_selection)) + (octave_range * 12)
             notes_dict[f'{note_selection}{octave_range - 2}'] = number
 
-def play_note(note, channel, output_reader):
-    note_on = mido.Message('note_on', note=notes_dict[note], channel=channel, velocity=100)
-    # with mido.open_output('virtualMidi 1', autoreset=True) as output:
-    output_reader.send(note_on)
-
-def turn_note_off(note, channel, output_reader):
-    note_off = mido.Message('note_off', note=notes_dict[note], channel=channel, velocity=100)
-    # with mido.open_output('virtualMidi 1', autoreset=True) as output:
-    output_reader.send(note_off)
-
-def modify_note(change_type, channel, output_reader, control_val=None, value=0):
-    if control_val:
-        note_mod = mido.Message(change_type, channel=channel, control=control_val, value=value)
-    output_reader.send(note_mod)
-
 def begin_monitoring():
+    global device_dict
     monitoring_layout = [[sg.Button('Start', key='-start-')],
                          [sg.Button('Stop', key='-stop-')]]
     monitoring_window = sg.Window('Monitoring Window', monitoring_layout)
@@ -89,12 +77,16 @@ def begin_monitoring():
         elif event == '-start-':
             total_uMyo_emg = []
             device_emg_data = {}
+            device_const_stream = {}
             for dev in device_dict.keys():
                 device_emg_data[dev] = []
+                device_const_stream[dev] = 0
+            print(device_emg_data)
             print(mido.get_output_names())
             output = mido.open_output('virtualMidi 1')
+            # input = mido.open_input('virtualMidi 0')
             time_start = datetime.now().timestamp()
-            print(f'Starting a 30 second monitoring window from {time_start}')
+            print(f'Starting a 90 second monitoring window from {time_start}')
             while True:
 
                 if datetime.now().timestamp() - time_start < 90:
@@ -112,39 +104,54 @@ def begin_monitoring():
                         # Loops through all devices to grab spcgm data specifically
                         for dev in devices:
                             device_id = dev.unit_id
+                            if device_id not in device_dict.keys():
+                                continue
                             active_device = device_dict[device_id]
                             emg_measurement = dev.device_spectr[1]
                             device_emg_data[active_device['Device ID']].append(emg_measurement)
                             total_uMyo_emg.append((device_id, emg_measurement))
                             buffer_length = 45
+                            try:
                             # Verifies buffer size is met in data stream
-                            if len(device_emg_data[active_device['Device ID']]) > buffer_length:
-                                # Sets buffer values
-                                buffer = device_emg_data[active_device['Device ID']][len(device_emg_data[active_device['Device ID']])-buffer_length:]
-                                # Checks if muscle is flexing based on buffer average
-                                buffer_value = sum(buffer)/len(buffer)
-                                if buffer_value > 300:
-                                    # Sends one note at a time based on muscle flexing
-                                    if not active_device['Active']:
-                                        play_note(active_device['Note'], active_device['Channel'], output)
-                                        active_device['Active'] = True
-                                    if active_device['Active']:
-                                        max_emg_val = max(device_emg_data[active_device['Device ID']][len(device_emg_data[active_device['Device ID']])-1500:])
-                                        # min_emg_val = min(device_emg_data[active_device['Device ID']][len(device_emg_data[active_device['Device ID']])-1500:])
-                                        min_emg_val = 300
-                                        print(f'Max Val: {max_emg_val}')
-                                        vol_ratio = (buffer_value - min_emg_val)/(max_emg_val - min_emg_val)
-                                        volume = int(math.sqrt((127**2)*(abs(vol_ratio))))
-                                        if volume >=127:
-                                            volume = 127
-                                        print(volume)
-                                        modify_note('control_change', active_device['Channel'], output, control_val=7, value=volume)
+                                if len(device_emg_data[active_device['Device ID']]) > buffer_length:
+                                    # Sets buffer values
+                                    buffer = device_emg_data[active_device['Device ID']][len(device_emg_data[active_device['Device ID']])-buffer_length:]
+                                    # Checks if muscle is flexing based on buffer average
+                                    buffer_value = sum(buffer)/len(buffer)
+                                    if buffer_value > 300:
+                                        # Sends one note at a time based on muscle flexing
+                                        if not active_device['Active']:
+                                            play_note(active_device['Note'], active_device['Channel'], output)
+                                            active_device['Active'] = True
+                                        if active_device['Active']:
+                                            # Create volume buffer for normalization
+                                            max_emg_val = max(device_emg_data[active_device['Device ID']][len(device_emg_data[active_device['Device ID']])-1500:])
+                                            # min_emg_val = min(device_emg_data[active_device['Device ID']][len(device_emg_data[active_device['Device ID']])-1500:])
+                                            min_emg_val = 300
+                                            vol_ratio = (buffer_value - min_emg_val)/(max_emg_val - min_emg_val)
+                                            volume = int(math.sqrt((127**2)*(abs(vol_ratio))))
+                                            if volume >= 127:
+                                                volume = 127
+                                            modify_note('control_change', active_device['Channel'], output, control_val=7, value=volume)
 
-                                        print(f'{active_device["Muscle"]} muscle is flexing')
-                                else:
-                                    turn_note_off(active_device['Note'], active_device['Channel'], output)
+                                            # Flip switch to track consecutive flexion sustain
+                                            device_const_stream[device_id] += 1
+                                            if device_const_stream[device_id] > 500:
+                                                flourish(active_device['Note'], active_device['Channel'], output)
+                                                ser.reset_input_buffer()
 
-                                    active_device['Active'] = False
+                                                device_emg_data[active_device['Device ID']] = []
+
+
+
+                                    else:
+                                        if active_device['Active']:
+                                            turn_note_off(active_device['Note'], active_device['Channel'], output)
+                                            device_const_stream[device_id] = 0
+                                            # active_device['Flourish State'] = False
+                                            active_device['Active'] = False
+                            except Exception as e:
+                                print(f'Error occurred: {e}')
 
                 else:
                     break
@@ -153,6 +160,35 @@ def begin_monitoring():
                     turn_note_off(device['Note'], device['Channel'], output)
             output.close()
     return total_uMyo_emg
+
+def play_note(note, channel, output_reader, velocity=100):
+    note_on = mido.Message('note_on', note=notes_dict[note], channel=channel, velocity=velocity)
+    # with mido.open_output('virtualMidi 1', autoreset=True) as output:
+    output_reader.send(note_on)
+
+def turn_note_off(note, channel, output_reader, velocity=100):
+    note_off = mido.Message('note_off', note=notes_dict[note], channel=channel, velocity=velocity)
+    # with mido.open_output('virtualMidi 1', autoreset=True) as output:
+    output_reader.send(note_off)
+
+def modify_note(change_type, channel, output_reader, control_val=None, value=0):
+    if control_val:
+        note_mod = mido.Message(change_type, channel=channel, control=control_val, value=value)
+    output_reader.send(note_mod)
+
+def flourish(base_note, channel, output):
+    pivot_root = base_note[0]
+    pivot_octave = int(base_note[-1])
+    pivot_scale = Note(pivot_root, pivot_octave)
+    pivot_primary, pivot_secondary, pivot_primary_triad, pivot_secondary_triad = pivot_scale.get_related_chords('minor')
+    # notes_queue = []
+    first_primary_triad = pivot_primary_triad[0]
+    for note in first_primary_triad:
+        # for note in chord:
+        play_note(note, channel, output)
+        time.sleep(.25)
+        turn_note_off(note, channel, output)
+    return first_primary_triad
 
 def main():
     init_globals()
